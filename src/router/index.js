0851ce6af192f13f3ac3914012bd0fd39a4d51e6
Arun Paul <arunpaul@2hats.com>
2025-02-03 14:53:10 +0530

    production issue fix - commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Home from '@/views/Home.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: process.env.API_BASE_URL,
+    baseURL: 'https://fakestoreapi.com/',
     headers: {
         'Content-Type': 'application/json'
     }
